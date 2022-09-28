9a7761dff3c21b557ca01cef31c26887bccd647d
Serkan ÖZAL <serkan@thundra.io>
2022-09-28 18:53:35 +0300

    Update TestMessageBuilder.java

--- src/test/java/com/mkyong/examples/TestMessageBuilder.java
+++ src/test/java/com/mkyong/examples/TestMessageBuilder.java
@@ -8,6 +8,7 @@ public class TestMessageBuilder {
 
     @Test
     public void testHelloWorld() {
+        System.out.println("testHelloWorld");
         assertEquals("hello world", MessageBuilder.getHelloWorld());
     }
 
