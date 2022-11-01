6161fefb14605f0849d3fc6645b39e816934f3af
erdoganforesight <89521820+erdoganforesight@users.noreply.github.com>
2022-11-01 10:57:56 +0300

    Update MessageBuilder.java

--- src/main/java/com/mkyong/examples/MessageBuilder.java
+++ src/main/java/com/mkyong/examples/MessageBuilder.java
@@ -5,6 +5,11 @@ public class MessageBuilder {
     public static String getHelloWorld(){
         return "hello world";
     }
+    
+    
+    public static String deneme(){
+        return "deneme";
+    }
 
     public static int getNumber10(){
         try {
