252cae73751e2868514534d1f86804860c70d56c
erdoganforesight <89521820+erdoganforesight@users.noreply.github.com>
2022-12-21 15:24:02 +0300

    Update TestMessageBuilder.java

--- src/test/java/com/mkyong/examples/TestMessageBuilder.java
+++ src/test/java/com/mkyong/examples/TestMessageBuilder.java
@@ -1,10 +1,13 @@
 package com.mkyong.examples;
 
 import org.junit.jupiter.api.Test;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestMessageBuilder {
+    
+    private Random random = new Random();
 
     @Test
     public void testHelloWorld() {
@@ -14,32 +17,51 @@ public class TestMessageBuilder {
     @Test
     public void testNumber10() {
         try { Thread.sleep(1000*3);} catch(Exception ex) {}
-        assertEquals(100, MessageBuilder.getNumber10());
+        if (rand.nextInt(10) < 2) {
+            assertEquals(100, MessageBuilder.getNumber10());
+        } else {
+            assertEquals(10, MessageBuilder.getNumber10());
+        }
     }
     
     @Test
     public void testNumber1001() {
         try { Thread.sleep(100*3);} catch(Exception ex) {}
-        assertEquals(100, MessageBuilder.getNumber10());
+        if (rand.nextInt(10) < 3) {
+            assertEquals(100, MessageBuilder.getNumber10());
+        } else {
+            assertEquals(10, MessageBuilder.getNumber10());
+        }
     }
     
-     
     @Test
     public void testNumber1002() {
         try { Thread.sleep(100*9);} catch(Exception ex) {}
-        assertEquals(10, MessageBuilder.getNumber10());
+        if (rand.nextInt(10) < 4) {
+            assertEquals(100, MessageBuilder.getNumber10());
+        } else {
+            assertEquals(10, MessageBuilder.getNumber10());
+        }
     }
     
       
     @Test
     public void testNumber1003() {
         try { Thread.sleep(100*9);} catch(Exception ex) {}
-        assertEquals(1002, MessageBuilder.getNumber10());
+        if (rand.nextInt(10) < 5) {
+            assertEquals(100, MessageBuilder.getNumber10());
+        } else {
+            assertEquals(10, MessageBuilder.getNumber10());
+        }
     }
     
     @Test
     public void testNumber100() {
         try { Thread.sleep(100*3);} catch(Exception ex) {}
-        assertEquals(100, MessageBuilder.getNumber10());
+        if (rand.nextInt(10) < 6) {
+            assertEquals(100, MessageBuilder.getNumber10());
+        } else {
+            assertEquals(10, MessageBuilder.getNumber10());
+        }
     }
 }
